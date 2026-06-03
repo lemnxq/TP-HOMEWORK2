7a6c404699a2c90a44bc4ecb0dac11466d262331
Арутюнян Тигран <tgarutyunyan@edu.hse.ru>
2026-06-04 00:18:57 +0300

    Добавлен .gitignore, заполнен readme.md и доработан тестировщик для ShoppingList

--- test_recipes.py
+++ test_recipes.py
@@ -1,5 +1,5 @@
 import pytest
-from recipes import Ingredient, Recipe
+from recipes import Ingredient, Recipe, ShoppingList
 
 
 def test_ingredient_init():
@@ -69,3 +69,65 @@ def test_recipe_len():
     recipe.add_ingredient(Ingredient("Мука", 100, "г")) 
     
     assert len(recipe) == 2
+
+
+def test_shopping_list_add_recipe():
+    sl = ShoppingList()
+    recipe = Recipe("Блины", [Ingredient("Мука", 100, "г")])
+    
+    sl.add_recipe(recipe, 2)
+    assert len(sl._items) == 1
+    assert sl._items[0][0].quantity == 200
+    assert sl._items[0][1] == "Блины"
+    
+    with pytest.raises(ValueError):
+        sl.add_recipe(recipe, 0)
+    with pytest.raises(ValueError):
+        sl.add_recipe(recipe, -2)
+
+def test_shopping_list_remove_recipe():
+    sl = ShoppingList()
+    r1 = Recipe("Блины", [Ingredient("Мука", 100, "г")])
+    r2 = Recipe("Омлет", [Ingredient("Яйца", 2, "шт")])
+    
+    sl.add_recipe(r1, 1)
+    sl.add_recipe(r2, 1)
+    
+
+    sl.remove_recipe("Блины")
+    assert len(sl._items) == 1
+    assert sl._items[0][1] == "Омлет"
+    
+    sl.remove_recipe("что-то")
+    assert len(sl._items) == 1
+
+def test_shopping_list_get_list():
+    sl = ShoppingList()
+    r1 = Recipe("Блины", [Ingredient("Мука", 100, "г"), Ingredient("Яйца", 2, "шт")])
+    r2 = Recipe("Хлеб", [Ingredient("Мука", 300, "г"), Ingredient("Вода", 100, "мл")])
+    
+    sl.add_recipe(r1, 1)
+    sl.add_recipe(r2, 1)
+    
+    final_list = sl.get_list()
+    
+    assert len(final_list) == 3
+    assert final_list[0].name == "Вода"
+    
+    assert final_list[1].name == "Мука"
+    assert final_list[1].quantity == 400
+    
+    assert final_list[2].name == "Яйца"
+
+def test_shopping_list_add_magic():
+    sl1 = ShoppingList()
+    sl1.add_recipe(Recipe("Чай", [Ingredient("Вода", 200, "мл")]), 1)
+    
+    sl2 = ShoppingList()
+    sl2.add_recipe(Recipe("Кофе", [Ingredient("Молоко", 50, "мл")]), 1)
+    
+    sl3 = sl1 + sl2
+    
+    assert len(sl3._items) == 2
+    assert len(sl1._items) == 1
+    assert len(sl2._items) == 1
